711914d5a58f02b222995fba2cfc8a81938b365e
sarukrishna <sarukrishnaprasad@gmail.com>
2017-12-11 22:09:38 +0530

    wiki new

--- wikiscrapper.py
+++ wikiscrapper.py
@@ -7,6 +7,8 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+import threading
+import time
 
 def webscrap(filepath):
     filecontent = open(filepath, "r", encoding='utf-8').readlines()
@@ -19,24 +21,33 @@ def webscrap(filepath):
             wiki = line.split()[-2].strip('<>').split('?')[0]
             wikilinks.add(wiki)
     for url in wikilinks:
-        crawl(url)
-    filecontent.close()        
-
+        t1 = threading.Thread(target=crawl, args=(url,))
+        #crawl(url)
+        t1.start()
+   
 def crawl(url):
-    urlobj = urllib.request.urlopen(url)
-    html = urlobj.read()
-    tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'p', 'li']
-    soup = BeautifulSoup(html, "lxml")
-    dirname = "images"
-    filename = url.split("/")[-1] + ".txt"
-    filename = os.path.join(dirname, filename)
-    for tag in soup.findAll(tags):
-        paragragph =  tag.get_text()
-        with open(filename, "a", encoding='utf-8') as f:
-            f.write(paragragph + "\n\n")
-    urlobj.close()
+    try:
+        urlobj = urllib.request.urlopen(url)
+        html = urlobj.read()
+        tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'p', 'li']
+        soup = BeautifulSoup(html, "lxml")
+        dirname = "images"
+        filename = url.split("/")[-1] + ".txt"
+        filename = os.path.join(dirname, filename)
+        for tag in soup.findAll(tags):
+            paragragph =  tag.get_text()
+            with open(filename, "a", encoding='utf-8') as f:
+                f.write(paragragph + "\n\n")
+        urlobj.close()
+    except Exception:
+        print("Exception occured " + str(url))
+
 if __name__ == "__main__":
+    start = time.time()
     webscrap("Sample_Peoples_Data.txt")
+    final = time.time()
+    total = final - start
+    print("total time of execution is " + str(total))
     
     
 	
