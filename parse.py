5b920e2381dca6550ab71d0330d98c1784aa1653
sarukrishna <sarukrishnaprasad@gmail.com>
2018-03-13 09:00:44 +0530

    Add files via upload

--- parse.py
+++ parse.py
@@ -3,32 +3,49 @@
 #read the content inside the link
 #write into a new file
 #
+import re
 from nltk import tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 def parse(file):
     name = file.split('/')[-1].split('.')[0].strip('tag')
-    ignore_tags = ['(', ')', '=', '-', '>', '<']
     f = open(file, 'r', encoding='charmap')
     content = f.readlines()
-    all_tags = ['he', 'his', 'him']
-    all_tags.append(name)
-    words = content[3].strip().split(' ')
-    for word in words:
-        if any(tag in word for tag in ignore_tags):
-            continue
-        else:
-            all_tags.append(word)
-    print(all_tags)
+    inputString = "Abraham Lincoln # President of # the United States of America"
+    inputSplit = word_tokenize(inputString)
+    stop_words = set(stopwords.words('english'))
+    filtered_sentense = [w for w in inputSplit if w not in stop_words]
+    #all_tags = ['he', 'his', 'him']
+
+    inputQuery = ' '.join(filtered_sentense).split(' # ')
+    subjent = inputQuery[0]
+    predicate = inputQuery[1]
+    objecT = inputQuery[2]
+
+    #inputQuery = ' '.join(inputQuery).split(' ')
+
+
+    print(inputQuery)
+   
+    #import pdb;pdb.set_trace()
+
     for line in content:
         if line.startswith('<') or line.startswith('=='):
             continue
         sentenses = tokenize.sent_tokenize(line)
         for sentense in sentenses:
-            if any(tag in sentense.lower() for tag in all_tags):
-                with open("output/" + name + "_filter.txt", "a", encoding='charmap') as f:
-                    f.write(sentense + '\n')
+            if (any(word in sentense for word in inputQuery[0].split(' ')) and 
+                any(word in sentense for word in inputQuery[1].split(' ')) and 
+                any(word in sentense for word in inputQuery[2].split(' '))):
+                if "TIMEX3" in sentense:
+                    match = re.findall(r'>(.*?)</TIMEX3>', sentense)
+                    if match:
+                        print(match)
+                        with open("output/" + name + "_filter.txt", "a", encoding='charmap') as f:
+                            f.write(str(sentense) + '\n' + str(match))
     f.close()
-     				
+         
 
 if __name__ == "__main__":
-    parse("output/Barack Obama tag.txt")
+    parse("output/Abraham Lincoln tag.txt")
